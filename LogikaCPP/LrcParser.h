eebac3525d455336dfaa9f3fac6cfa4c90ec0441
muhammadfadlan31651-collab <muhammad.fadlan31651@sma.belajar.id>
2026-05-17 16:03:31 +0700

    Update LrcParser.h

--- LogikaCPP/LrcParser.h
+++ LogikaCPP/LrcParser.h
@@ -1,1 +1,22 @@
+#pragma once
+#include <QString>
+#include <QVector>
+
+struct BariLirik {
+    qint64  waktu_ms;
+    QString teks;
+};
+
+struct InfoLagu {
+    QString judul, penyanyi, album;
+    int offset_ms = 0;
+};
+
+QVector<BariLirik> bacaLRC(const QString& path, InfoLagu* info = nullptr);
+QVector<BariLirik> bacaSRT(const QString& path);
+QVector<BariLirik> bacaTXT(const QString& path);
+QVector<BariLirik> parseStringLRC(const QString& isi, InfoLagu* info = nullptr);
+
+qint64 lrcKeMs(const QString& t);
+qint64 srtKeMs(const QString& t);
 
